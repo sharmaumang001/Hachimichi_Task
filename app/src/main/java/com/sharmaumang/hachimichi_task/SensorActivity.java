1b020e2f4b62ac5ece44df3371e68fd4023cede7
umang <iumangsharma001@gmail.com>
2020-06-27 12:34:05 +0530

    First Activity created and Upload added.

--- app/src/main/java/com/sharmaumang/hachimichi_task/SensorActivity.java
+++ app/src/main/java/com/sharmaumang/hachimichi_task/SensorActivity.java
@@ -1,14 +1,14 @@
-//package com.sharmaumang.hachimichi_task;
-//
-//import androidx.appcompat.app.AppCompatActivity;
-//
-//import android.os.Bundle;
-//
-//public class SensorActivity extends AppCompatActivity {
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_sensor);
-//    }
-//}
+package com.sharmaumang.hachimichi_task;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SensorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sensor);
+    }
+}
